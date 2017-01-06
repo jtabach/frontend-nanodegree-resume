ac21600e36da91ce4d1dc994db5ba0cea90a3630
jtabach <jeff.n.tabachnick@gmail.com>
2017-01-06 08:19:29 -0800

    append bio to resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,21 +1,39 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-// var bio = {
-//   name: "Jeff Tabachnick",
-//   role: "Front End Engineer",
-//   contacts: {
-//     mobile: "818 292-0405",
-//     email: "jeff.n.tabachnick@gmail.com",
-//     github: "jtabach",
-//     twitter: "@jtabach",
-//     location: "San Francisco"
-//   },
-//   welcomeMessage: "Hello, my name a Jeff!",
-//   skills: ["Hockey", "Snowboarding", "Guitar", "Acting"],
-//   biopic: "What?",
-//   display: function() {}
-// };
+var bio = {
+  name: "Jeff Tabachnick",
+  role: "Front End Engineer",
+  contacts: {
+    mobile: "818 292-0405",
+    email: "jeff.n.tabachnick@gmail.com",
+    github: "jtabach",
+    twitter: "@jtabach",
+    location: "San Francisco"
+  },
+  welcomeMessage: "Hello, my name a Jeff!",
+  skills: ["Hockey", "Snowboarding", "Guitar", "Acting"],
+  biopic: "images/fry.jpg",
+  display: function() {
+    $('#header').prepend(replaceHolder(this.name, HTMLheaderName)
+      .concat(replaceHolder(this.role, HTMLheaderRole))
+    ).append(replaceHolder(this.biopic, HTMLbioPic)
+      .concat(replaceHolder(this.welcomeMessage, HTMLwelcomeMsg))
+      .concat(HTMLskillsStart)
+    );
+    $("#skills").append(
+      this.skills.map(function(skill, i) {
+        return replaceHolder(skill, HTMLskills)
+      })
+    );
+    $('#topContacts, #footerContacts').append(replaceHolder(bio.contacts.mobile, HTMLmobile)
+      .concat(replaceHolder(this.contacts.email, HTMLemail))
+      .concat(replaceHolder(this.contacts.github, HTMLgithub))
+      .concat(replaceHolder(this.contacts.twitter, HTMLtwitter))
+      .concat(replaceHolder(this.contacts.location, HTMLlocation))
+    );
+  }
+};
 
 
 var work = {
@@ -37,7 +55,7 @@ var work = {
   ],
   display: function() {
     $("#workExperience").append(
-      work.jobs.map(function(job, i) {
+      this.jobs.map(function(job, i) {
         return $(HTMLworkStart)
           .append(replaceHolder(job.employer, HTMLworkEmployer)
             .concat(replaceHolder(job.title, HTMLworkTitle))
@@ -50,6 +68,7 @@ var work = {
   }
 };
 
+bio.display();
 work.display();
 
 function replaceHolder(value, helper, placeholder) {
